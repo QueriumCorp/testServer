a2eee50e48fcee0a174514ee76950ee1f171ffd9
Evan <igobyE@gmail.com>
2020-08-03 14:34:04 -0600

    init repo

---
+++ test.py
@@ -0,0 +1,10 @@
+###############################################################################
+# test.py
+# Testing module
+###############################################################################
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+def helloWorld():
+    print("Hello World!")
