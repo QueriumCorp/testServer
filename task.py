e04847b0093f045fadf6f6066dd2f27023e3220a
Evan <igobyE@gmail.com>
2021-02-09 00:07:41 -0600

    To prevent collision in acquiring the same task among multiple testServers, update status, host, and pid fields of a task, and then fetch it

--- task.py
+++ task.py
@@ -2,50 +2,63 @@
 # test.py
 # Testing module
 ###############################################################################
+import sys
+import util
+from subprocess import TimeoutExpired
+import subprocess
+import os
+import logging
+import dbConn
+from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
-from datetime import datetime
-import dbConn
-import logging
-import os
-import subprocess
-from subprocess import TimeoutExpired
-import util
-import sys
+
+# To prevent multiple testServers acquiring the same task, change
+# status, host, and pid fields to "acquired", serverHost, and process ID,
+# respectively. Then get the task
 
 
 def next(statusCurr="pending", statusNext="acquired", queryAdd="LIMIT 1"):
     tbl = "testPath"
+
+    # Update status and started fields
+    pid = os.getpid()
+    dbConn.modMultiVals(
+        tbl,
+        ["status"], [statusCurr],
+        ["status", "host", "pid"],
+        [statusNext, os.environ.get('serverHost'), pid],
+        fltr="LIMIT 1"
+    )
+    logging.debug("task-next: before fetching a task, update a task with {status} status, {host} host, and {pid} pid".format(status=statusNext, host=os.environ.get('serverHost'), pid=pid))
+
+    # Get the task
     colsRtrn = dbConn.getFields("testPath")
     rslt = dbConn.getRow(
         tbl,
-        ["status"], [statusCurr],
+        ["status", "host", "pid"],
+        [statusNext, os.environ.get('serverHost'), pid],
         colsRtrn, queryAdd)
-    ## If no pending task return {}
-    if rslt==None or len(rslt)<1:
+    # If no pending task return {}
+    if rslt == None or len(rslt) < 1:
         return []
-    ## Make a dictionary of the result
+
+    # Make a dictionary of the result
     rslt = dbConn.mkObj(colsRtrn, rslt[0])
     logging.debug("task-next id: {}".format(rslt["id"]))
 
-    ## Update status and started fields
-    dbConn.modMultiVals(
-        tbl,
-        ["id"], [rslt["id"]],
-        ["status", "host", "pid"],
-        [statusNext, os.environ.get('serverHost'), os.getpid()]
-    )
     return rslt
+
 
 def run(aTask):
     logging.debug('run tasks')
     logging.debug(aTask)
 
-    ## Remove some fields
+    # Remove some fields
     for f in ['started', 'finished']:
         aTask.pop(f)
 
-    ## Run the testing script on a task
+    # Run the testing script on a task
     try:
         subprocess.run([
             os.environ.get("wolframscript"),
