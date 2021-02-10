534cc9022a069c647279a22fc6da7301c2ceb1b4
Evan <igobyE@gmail.com>
2021-02-09 21:47:36 -0600

    Updated fields in testSchedule and testPath tables

--- dbConn.py
+++ dbConn.py
@@ -38,12 +38,12 @@ def getFields(tbl):
     switcher = {
         "testSchedule": [
             "id","name","jira","author","gradeStyle","policies","skipStatuses",
-            "status","limitPaths","limitStepTime","limitSteps","limitPathTime","host","pid","gitBranch","gitHash","mmaVersion","timeOutTime",
+            "status","limitPaths","priority","limitPathTime","host","pid","gitBranch","gitHash","mmaVersion","timeOutTime",
             "ruleMatchTimeOutTime","msg","jiraResp","started","finished","created"
             ],
         "testPath": [
             "id","name","question_id","path_id","trace_id","diff_id","author",
-            "gradeStyle","policies","status","limitStepTime","limitSteps",
+            "gradeStyle","policies","status","ref_id","priority",
             "limitPathTime","pid","stepCount","stepsCompleted","timeCompleted",
             "host","gitBranch","gitHash","mmaVersion","timeOutTime",
             "ruleMatchTimeOutTime","msg","started","finished"
