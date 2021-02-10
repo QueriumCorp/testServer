34a6c331ad8eabf7b33c9a81a1932af93dc3472b
Evan <igobyE@gmail.com>
2021-02-10 17:08:25 -0600

    getFields: updated fields in testSchedule and testPath

--- dbConn.py
+++ dbConn.py
@@ -37,17 +37,14 @@ def mkObjs(keys, data):
 def getFields(tbl):
     switcher = {
         "testSchedule": [
-            "id","name","jira","author","gradeStyle","policies","skipStatuses",
-            "status","limitPaths","limitStepTime","limitSteps","limitPathTime","host","pid","gitBranch","gitHash","mmaVersion","timeOutTime",
-            "ruleMatchTimeOutTime","msg","jiraResp","started","finished","created"
-            ],
+            "id", "name", "jira", "author", "gradeStyle", "policies", "skipStatuses", "status", "limitPaths", "priority", "limitPathTime", "host", "pid", "gitBranch", "gitHash", "mmaVersion", "timeOutTime",
+            "ruleMatchTimeOutTime", "msg", "jiraResp", "started", "finished", "created"
+        ],
         "testPath": [
-            "id","name","question_id","path_id","trace_id","diff_id","author",
-            "gradeStyle","policies","status","limitStepTime","limitSteps",
-            "limitPathTime","pid","stepCount","stepsCompleted","timeCompleted",
-            "host","gitBranch","gitHash","mmaVersion","timeOutTime",
-            "ruleMatchTimeOutTime","msg","started","finished"
-            ]
+            "name", "question_id", "path_id", "trace_id", "diff_id", "author",
+            "gradeStyle", "policies", "status", "ref_id", "priority",
+            "limitPathTime", "pid", "stepCount", "stepsCompleted", "timeCompleted", "host", "gitBranch", "gitHash", "mmaVersion", "timeOutTime", "ruleMatchTimeOutTime", "msg", "started", "finished"
+        ]
     }
 
     return switcher.get(tbl, [])
